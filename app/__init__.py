6af8e3d2d91cb3b2f4b6061593b000aa4807497b
RachelMinto <rachel.j.minto@gmail.com>
2017-03-05 18:48:49 -0500

    Added in User and Post tables as well as migration files.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
+db = SQLAlchemy(app)
 
-from app import views # this syntax avoids circular import error
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+from app import views, models # this syntax avoids circular import error
