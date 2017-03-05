922b698c984672cc11196fccbf10a952d8305eaf
RachelMinto <rachel.j.minto@gmail.com>
2017-03-05 13:24:16 -0500

    First templates.

--- app/views.py
+++ app/views.py
@@ -1,6 +1,21 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Rachel'}
+    posts = [
+        {
+            'author': {'nickname': 'Jerry'},
+            'body': 'Beautiful day in Boston!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool.'
+        }
+    ]
+    return render_template('index.html',
+                            title='Home',
+                            user=user,
+                            posts=posts)
