87ed5597a5c0bc9edbd2dd6a0ba6fe8dfe721375
RachelMinto <rachel.j.minto@gmail.com>
2017-03-12 16:49:04 -0400

    Working login using user that persists to db.

--- app/views.py
+++ app/views.py
@@ -1,8 +1,12 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask_login import login_user, logout_user, current_user, login_required
-from app import app, db, lm, oid
+from app import app, db, lm
 from .forms import LoginForm
 from .models import User
+
+# @login_manager.user_loader
+# def load_user(user_id):
+#     return User.get(user_id)
 
 @app.before_request
 def before_request():
@@ -29,25 +33,46 @@ def index():
                             posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
-@oid.loginhandler
 def login():
-    if g.user is not None and g.user.is_authenticated:
-        return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        session['remember_me'] = form.remember_me.data
-        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
-    return render_template('login.html', 
-                           title='Sign In',
-                           form=form,
-                           providers=app.config['OPENID_PROVIDERS'])
+    error = None
+    if request.method == 'POST':
+        user = User.query.filter_by(nickname=request.form['nickname']).first()
+        if user and request.form['password'] == user.password:
+            session['logged_in'] = True
+            flash('You have been successfully logged in.')
+            posts = [
+                {
+                    'author': {'nickname': 'Jerry'},
+                    'body': 'Beautiful day in Boston!'
+                },
+                {
+                    'author': {'nickname': 'Susan'},
+                    'body': 'The Avengers movie was so cool.'
+                }
+            ]            
+            return render_template('index.html',
+                                    title='Home',
+                                    user=user,
+                                    posts=posts)
+    return render_template('login.html', error=error)
 
 @app.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for('login'))
 
-@oid.after_login
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    error = None
+    if request.method == 'POST':    
+        user = User(request.form['nickname'], request.form['email'], request.form['password'])
+        db.session.add(user)
+        print user
+        db.session.commit()
+        flash('You have been successfully registered. Please login.')
+        return redirect('/login')
+    return render_template('register.html', error=error)        
+
 def after_login(resp):
     if resp.email is None or resp.email == "":
         flash('Invalid login. Please try again.')
