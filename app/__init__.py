41eebfcd8d3a16464edeba1929b746f81de0d0d0
RachelMinto <rachel.j.minto@gmail.com>
2017-03-12 19:25:59 -0400

    Working on part vii Sending Errors Via Email.

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 import os
 from flask_login import LoginManager
-from config import basedir
 
-import application
-
+from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -19,3 +17,12 @@ lm.login_view = 'login'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 from app import views, models # this syntax avoids circular import error
 
+if not app.debug:
+    import logging
+    from logging.handlers import SMTPHandler
+    credentials = None
+    if MAIL_USERNAME or MAIL_PASSWORD:
+        credentials = (MAIL_USERNAME, MAIL_PASSWORD)
+    mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT), 'no-reply@' + MAIL_SERVER, ADMINS, 'microblog failure', credentials)
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
