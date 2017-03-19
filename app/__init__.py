c8357810b8ce6e9b022e98634d665fd7b59bf07b
RachelMinto <rachel.j.minto@gmail.com>
2017-03-19 11:57:31 -0400

    Added safeguard for dup nickname, and added unit tests.

--- app/__init__.py
+++ app/__init__.py
@@ -20,6 +20,13 @@ from app import views, models # this syntax avoids circular import error
 if not app.debug:
     import logging
     from logging.handlers import SMTPHandler
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler('tmp/microblog.log', 'a', 1 * 1024 * 1024, 10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('microblog startup')
     credentials = None
     if MAIL_USERNAME or MAIL_PASSWORD:
         credentials = (MAIL_USERNAME, MAIL_PASSWORD)
